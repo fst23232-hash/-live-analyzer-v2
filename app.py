6cc79d330d4e8d0cf09e3fc0475a0c02f89bca0e
fst23232-hash <fst23232@gmail.com>
2026-09-18 20:45:08 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -378,12 +378,11 @@ if st.button(
 
 
 # Atualização automática
-if "jogos" not in st.session_state:
+if st.button(
+    "🔄 ATUALIZAR JOGOS",
+    use_container_width=True
+):
     st.session_state["jogos"] = buscar_jogos_ao_vivo()
-else:
-    st.session_state["jogos"] = buscar_jogos_ao_vivo()
-
-
 # ==============================
 # EXIBIÇÃO
 # ==============================
