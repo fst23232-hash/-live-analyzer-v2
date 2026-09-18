ce91cd34e8e3463041436830a58555c3a771eab5
fst23232-hash <fst23232@gmail.com>
2026-09-18 19:30:21 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -34,7 +34,13 @@ def buscar_jogos_ao_vivo():
             url,
             timeout=10,
             headers={
-                "User-Agent": "Mozilla/5.0"
+                headers={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/plain, */*",
+    "Referer": "https://www.sofascore.com/",
+    "Origin": "https://www.sofascore.com",
+    "X-Requested-With": "XMLHttpRequest"
+}
             }
         )
 
